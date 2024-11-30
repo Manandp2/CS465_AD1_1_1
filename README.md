c2b449ca8b29db4d7ed025fc943535c646a30d3a
Tiffany Yu <yutiff25@gmail.com>
2024-11-29 20:40:57 -0600

    Updated README + HomeBottomBar

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CS465_AD1_1_1
+
+## Quick Start
+To compile on your local machine, try running these commands in the terminal within src/calendone:
+- npm install
+- npm install google-calendar-react
+- npm i gapi-script
+- npm start
+- npm start react-router-dom
+- npm install @material/web
--- calendone/src/components/HomeBottombar.js
+++ calendone/src/components/HomeBottombar.js
@@ -5,11 +5,12 @@ import '@material/web/tabs/primary-tab.js';
 import '@material/web/tabs/secondary-tab.js';
 import '@material/web/tabs/tabs.js';
 
+// Icons
+// import '@material-ui/icons/Settings';
+
 export default function HomeBottombar() {
     return (
         <div>
-            <p>Insert bottom bar here</p>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=settings" />
             <md-tabs>
                 <md-primary-tab>Settings</md-primary-tab>
                 <md-primary-tab>Add</md-primary-tab>
