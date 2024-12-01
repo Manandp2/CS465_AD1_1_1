752982446a06f451da090a2c0b3f0e73f86683ea
Kyu Hong Kim <03kkim@gmail.com>
2024-11-30 23:06:34 -0600

    Replace react router with simple useState and also replace Edit Icon with MUI icon

--- calendone/src/App.js
+++ calendone/src/App.js
@@ -1,26 +1,22 @@
-import './App.css';
-import { 
-  BrowserRouter as Router, 
-  Routes,
-  Route
-} from 'react-router-dom';
-import Home from './pages/home/Home';
-import Settings from './pages/settings/Settings';
+import "./App.css";
+import { useState } from "react";
+import Home from "./pages/home/Home";
+// import Settings from './pages/settings/Settings';
 
 function App() {
-  return (
-    <div>
-      {/* Material Design Icons import */}
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/settings" element={<Settings />}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+  const [curPage, setCurPage] = useState("Home");
+  if (curPage === "Home") {
+    return <Home />;
+  }
+  // return (
+  //   <div>
+  //     {/* Material Design Icons import */}
+  //     <link
+  //       rel="stylesheet"
+  //       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+  //     />
+  //   </div>
+  // );
 }
 
 export default App;
