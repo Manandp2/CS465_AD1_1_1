a43e1626970accda037a7a1b3fc5217382f260d4
Tiffany Yu <yutiff25@gmail.com>
2024-12-03 17:34:21 -0600

    Updated with mui branch

--- calendone/src/components/Bottombar.js
+++ calendone/src/components/Bottombar.js
@@ -2,13 +2,23 @@ import React from "react";
 
 import { Paper, Typography } from "@mui/material";
 
-import IconButton from "@mui/material/IconButton";
-import SettingsIcon from '@mui/icons-material/Settings';
-
-export default function Bottombar() {
+export default function Topbar({ header }) {
   return (
-    <Paper square elevation={3} sx={{ backgroundColor: "#6d3b79", color: "white", paddingY: "3%", position: 'fixed', bottom: 0, left: 0, right: 0, textAlign: "center" }}>
-      <IconButton sx={{ color: "white" }}><SettingsIcon /></IconButton>
-    </Paper>
+    <Paper
+        square
+        elevation={3}
+        sx={{
+          backgroundColor: "#6d3b79",
+          color: "white",
+          paddingY: "3%",
+          position: "fixed",
+          bottom: "0",
+          left: 0,
+          right: 0,
+          textAlign: "center"
+        }}
+      >
+        <Typography variant="h4">"Bottom Bar"</Typography>
+      </Paper>
   );
 }
--- calendone/src/pages/home/Home.js
+++ calendone/src/pages/home/Home.js
@@ -1,22 +1,22 @@
-import "./Home.css";
+// import "./Home.css";
 import React from "react";
 import { useState } from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
 
 import Topbar from "../../components/Topbar";
 import Bottombar from "../../components/Bottombar";
+// import HomeTab from "../../components/tabs/HomeTab";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import TaskList from "../../components/TaskList";
 
-import PropTypes from "prop-types";
-
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { CssBaseline, Paper } from "@mui/material";
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -52,12 +52,12 @@ export default function Home() {
   // const [activeTabIndex, setActiveTabIndex] = useState(0);
 
   const unschedList = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     unschedList.push("Unscheduled Task " + i);
   }
 
   const schedList = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     schedList.push("Scheduled Task " + i);
   }
 
@@ -68,33 +68,35 @@ export default function Home() {
   // const [tab, setTab] = React.useState(0);
 
   return (
-    <>
+    <div>
+      <Paper square elevation={3} sx={{ backgroundColor: "white", color: "white", paddingY: "3%" }}>
+        <Typography variant="h4">PLACEHOLDER</Typography>
+      </Paper>
+      <Paper sx={{ overflowY: "scroll" }}>
+        <Accordion disableGutters>
+          <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel1-content" id="panel1-header">
+            <Typography>Unscheduled Tasks</Typography>
+          </AccordionSummary>
+          <AccordionDetails sx={{ paddingX: "0" }}>
+            <Typography>
+              <TaskList taskList={unschedList} />
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel2-content" id="panel2-header">
+            <Typography>Scheduled Tasks</Typography>
+          </AccordionSummary>
+          <AccordionDetails sx={{ paddingX: "0" }}>
+            <Typography>
+              <TaskList taskList={schedList} />
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Paper>
+
       <Topbar header={"CalenDone"} />
-
-      {/* <HomeTab onSet={(num) => setActiveTabIndex(num)} /> */}
-
-      <Accordion disableGutters>
-        <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel1-content" id="panel1-header">
-          <Typography>Unscheduled Tasks</Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ paddingX: "0" }}>
-          <Typography>
-            <TaskList taskList={unschedList} />
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary expandIcon={<ArrowDropDownIcon />} aria-controls="panel2-content" id="panel2-header">
-          <Typography>Scheduled Tasks</Typography>
-        </AccordionSummary>
-        <AccordionDetails sx={{ paddingX: "0" }}>
-          <Typography>
-            <TaskList taskList={schedList} />
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-
       <Bottombar />
-    </>
+    </div>
   );
 }
