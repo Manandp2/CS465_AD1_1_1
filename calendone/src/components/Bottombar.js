501ff917ef26840ae489e481110af89d91382219
Tiffany Yu <yutiff25@gmail.com>
2024-12-03 17:27:09 -0600

    Updated top and bottombar

--- calendone/src/components/Bottombar.js
+++ calendone/src/components/Bottombar.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "@material/web/icon/icon.js";
-import "@material/web/iconbutton/icon-button.js";
 
 import { Paper, Typography } from "@mui/material";
 
-export default function Bottombar({ header }) {
+import IconButton from "@mui/material/IconButton";
+import SettingsIcon from '@mui/icons-material/Settings';
+
+export default function Bottombar() {
   return (
-    <Paper square elevation={3} sx={{ backgroundColor: "#6d3b79", color: "white", paddingY: "3%", position: 'fixed', bottom: 0, left: 0, right: 0 }}>
-      <Typography variant="h4">{header}</Typography>
+    <Paper square elevation={3} sx={{ backgroundColor: "#6d3b79", color: "white", paddingY: "3%", position: 'fixed', bottom: 0, left: 0, right: 0, textAlign: "center" }}>
+      <IconButton sx={{ color: "white" }}><SettingsIcon /></IconButton>
     </Paper>
   );
 }
