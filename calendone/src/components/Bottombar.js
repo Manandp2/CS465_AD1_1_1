9ae0467c70f1b7ce600dccdee9a9307390c8c23b
Kyu Hong Kim <03kkim@gmail.com>
2024-12-03 17:54:11 -0600

    Fix bottombar

--- calendone/src/components/Bottombar.js
+++ calendone/src/components/Bottombar.js
@@ -1,30 +1,45 @@
 import React from "react";
 
-import { Paper, Typography, IconButton } from "@mui/material";
+import { Paper, Typography, IconButton, Stack } from "@mui/material";
 
-import SettingsIcon from '@mui/icons-material/Settings';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import ChecklistIcon from '@mui/icons-material/Checklist';
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import ChecklistIcon from "@mui/icons-material/Checklist";
 
 export default function Bottombar() {
   return (
     <Paper
-        square
-        elevation={3}
+      square
+      elevation={3}
+      sx={{
+        backgroundColor: "#6d3b79",
+        color: "white",
+        paddingY: "1%",
+        position: "fixed",
+        bottom: "0",
+        left: 0,
+        right: 0,
+        textAlign: "center",
+      }}
+    >
+      <Stack
+        direction="row"
+        spacing={0}
         sx={{
-          backgroundColor: "#6d3b79",
-          color: "white",
-          paddingY: "1%",
-          position: "fixed",
-          bottom: "0",
-          left: 0,
-          right: 0,
-          textAlign: "center"
+          justifyContent: "space-around",
+          alignItems: "center",
         }}
       >
-        <IconButton sx={{ color: "white" }}><SettingsIcon fontSize="large" /></IconButton>
-        <IconButton sx={{ color: "white" }}><AddCircleOutlineIcon fontSize="large" /></IconButton>
-        <IconButton sx={{ color: "white" }}><ChecklistIcon fontSize="large" /></IconButton>
-      </Paper>
+        <IconButton sx={{ color: "white" }}>
+          <SettingsIcon sx={{ fontSize: "170%" }} />
+        </IconButton>
+        <IconButton sx={{ color: "white" }}>
+          <AddCircleOutlineIcon sx={{ fontSize: "170%" }} />
+        </IconButton>
+        <IconButton sx={{ color: "white" }}>
+          <ChecklistIcon sx={{ fontSize: "170%" }} />
+        </IconButton>
+      </Stack>
+    </Paper>
   );
 }
